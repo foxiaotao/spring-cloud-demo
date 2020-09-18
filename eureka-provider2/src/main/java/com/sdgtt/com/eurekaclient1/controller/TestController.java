7dc8158270ef9518976b6540671bdeb73a0625be
suntao <384631173@qq.com>
2020-09-18 18:30:32 +0800

    feign（ribbon）重试策略

--- eureka-provider2/src/main/java/com/sdgtt/com/eurekaclient1/controller/TestController.java
+++ eureka-provider2/src/main/java/com/sdgtt/com/eurekaclient1/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.sdgtt.com.eurekaclient1.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,11 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @author tao
  * @version 2020-09-17 14:43
  */
+@Slf4j
 @RestController
 public class TestController {
 
     @GetMapping("/hello")
     public Object hello(){
+        log.info("service provider 2");
         return "hello,I'am service provider 2";
     }
 }
